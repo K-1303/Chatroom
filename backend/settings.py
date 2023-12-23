a037e21023a4b1568d9cf0dda1bb8bb08985db36
Kavya Dua <114342842+K-1303@users.noreply.github.com>
2023-12-24 01:59:28 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://chatroom-9osn.onrender.com',]
+CSRF_TRUSTED_ORIGINS = ['https://chatroom-task.onrender.com',]
 
 
 # Application definition
